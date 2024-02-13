8c17729e0beb23803039d259a64dca8bcd20d77a
Manvi Arora <manvi.arora2603@gmail.com>
2024-02-13 15:37:19 +0530

    Script Updated

--- monitoring.sh
+++ monitoring.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 #echo "Updating..."
+
 #sudo apt update
+if [[ $EUID -ne 0 ]]; then
+  echo "This script requires root privileges. Please run as sudo."
+  exit 1
+fi
+
+echo "Installing the latest Node.js version..." >> ~/errors.txt
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 20
+nvm use 20
+
 echo "Installing Node"
 sudo apt install nodejs
 echo "Updating Node to latest"
@@ -15,17 +27,31 @@ echo "npm version: $npm_version"
 echo ""
 echo "----------------Installing Monitoring Tool UPTIME-KUMA----------------"
 echo ""
-git clone https://github.com/louislam/uptime-kuma.git
+echo "Cloning Uptime-Kuma..." >> ~/errors.txt
+
+if ! git clone https://github.com/louislam/uptime-kuma.git; then
+  echo "Error cloning Uptime-Kuma. Please check connectivity and repository URL."
+  exit 1
+fi
+
+sudo git clone https://github.com/louislam/uptime-kuma.git
 echo "Providing Permissions"
 sudo chown -R $USER:$USER uptime-kuma/
-sleep 5
 cd uptime-kuma/
-echo "setting up the app"
+
+echo "Setting up the app"
+
 npm run setup
-sleep 20
+
 #node server/server.js
+
+echo "Installing PM2"
+
 npm install pm2 -g
+
 pm2 start server/server.js --name uptime-kuma
+
 pm2 save
-echo "ending"
+
+echo "-------------------------Script Ended-----------------------------"
 
