14665907a87eec5f21f420fc45936d7d1eed5492
Qu4ntumGuy <gauravbisht8000@gmail.com>
2024-02-22 18:39:48 +0530

    updation

--- .history/monintoring-kuma/add_monitor_20240222183153.py
+++ .history/monintoring-kuma/add_monitor_20240222183153.py
@@ -1,23 +1,22 @@
 import subprocess
-from uptime_kuma_api import UptimeKumaApi, MonitorType
+from uptime_kuma_api import UptimeKumaApi,MonitorType
 # Replace ['your', 'command', 'here'] with the actual command and its arguments
 
 command = ['wget', '-qO-', 'https://api64.ipify.org']
 
 try:
-    result = subprocess.run(
-        command, capture_output=True, text=True, check=True)
+    result = subprocess.run(command, capture_output=True, text=True, check=True)
     print(result.stdout)
 except subprocess.CalledProcessError as e:
     print(f"Error: {e}")
     print(f"Output: {e.output}")
 
 sending_url = "http://" + result.stdout
-
-with UptimeKumaApi('http://18.220.82.58') as api:
+ 
+with UptimeKumaApi('http://3.145.70.251') as api:
     api.login('admin', 'root@123')
     api.add_monitor(
         type=MonitorType.HTTP,
         name="Client-sever",
         url=sending_url,
-    )
+        )
